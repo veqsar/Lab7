3cb928e6f01d02670f18323752b75a54b519b29c
Ihor Shcherbina <hoqqez@gmail.com>
2024-01-19 19:41:27 +0200

    editing code

--- lab1.cpp
+++ lab1.cpp
@@ -1,13 +1,12 @@
 ﻿#include <iostream>
 #include <iomanip>
-using namespace std;
 
 int main()
 {
-    cout << setw(10) << showpos << +4 << endl;
-    cout << setw(15) << internal << -67.09 << endl;
-    cout << setw(10) << right << +235 << endl;
-    cout << noshowpos << 8 << "a1" << endl;
-    cout << fixed << setprecision(2) << -121.00 << endl;
-    cout << showpos << +1 << showpos << +24 << endl;
+    std::cout << setw(10) << showpos << +4 << endl;
+    std::cout << setw(15) << internal << -67.09 << endl;
+    std::cout << setw(10) << right << +235 << endl;
+    std::cout << noshowpos << 8 << "a1" << endl;
+    std::cout << fixed << setprecision(2) << -121.00 << endl;
+    std::cout << showpos << +1 << showpos << +24 << endl;
 }
