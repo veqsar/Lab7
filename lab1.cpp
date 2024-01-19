25a6feb4c04102f42cf4930b53e9ca050428e2ae
Ihor Shcherbina <hoqqez@gmail.com>
2024-01-19 19:37:38 +0200

    something added

---
+++ lab1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << setw(10) << showpos << +4 << endl;
+    cout << setw(15) << internal << -67.09 << endl;
+    cout << setw(10) << right << +235 << endl;
+    cout << noshowpos << 8 << "a1" << endl;
+    cout << fixed << setprecision(2) << -121.00 << endl;
+    cout << showpos << +1 << showpos << +24 << endl;
+}
